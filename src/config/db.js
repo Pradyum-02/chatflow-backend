9e234deb14e774f763f03b2effdf33cd8097778c
Pradyum-02 <pradyumofc13@gmail.com>
2026-08-02 19:29:40 +0530

    Fix MongoDB environment variable

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,9 @@
-
 const mongoose = require("mongoose");
-console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
-console.log("JWT exists:", !!process.env.JWT_SECRET);
 
 const connectDB = async () => {
     try {
 
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGO_URI);
 
         console.log("✅ MongoDB Connected");
 
